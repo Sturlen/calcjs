e50198cff467faf78e4e69d2aeaaae3197f7b104
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-06-01 17:32:12 +0200

    Adding use case test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,45 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import App from "./App"
+import { act } from "react-dom/test-utils"
 
-test("renders learn react link", () => {
+test("renders without crashing", () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const inputBox = getByText(/1/i)
+  expect(inputBox).toBeInTheDocument()
+})
+
+test("One plus Two equals Three", async () => {
+  const { findByText, findByTitle } = render(<App />)
+
+  const onebutton = await findByText("1")
+  const twobutton = await findByText("2")
+  const plusbutton = await findByText("+")
+  const equalsbutton = await findByText("=")
+  const inputbox = await findByTitle(/result/i)
+  const historybox = await findByTitle(/history/i)
+
+  act(() => {
+    onebutton.click()
+  })
+  expect(inputbox.textContent).toBe("1")
+
+  act(() => {
+    plusbutton.click()
+  })
+  expect(inputbox.textContent).toBe("1")
+  expect(historybox.textContent).toBe("1+")
+
+  act(() => {
+    twobutton.click()
+  })
+  expect(inputbox.textContent).toBe("2")
+  expect(historybox.textContent).toBe("1+")
+
+  act(() => {
+    equalsbutton.click()
+  })
+
+  expect(inputbox.textContent).toBe("3")
+  expect(historybox.textContent).toBe("1+2=")
 })
